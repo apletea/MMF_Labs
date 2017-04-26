92bb440b6f211443b3efc663020a19938b317b98
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-04-27 02:46:23 +0300

    push

--- src/lab7/B.java
+++ src/lab7/B.java
@@ -1,7 +1,27 @@
 package lab7;
 
+import java.util.Scanner;
+
 /**
  * Created by apletea on 6.4.17.
  */
 public class B {
+     public static void main(String ... args){
+         Scanner sc = new Scanner(System.in);
+         String str = sc.nextLine();
+         String strs[] = str.split(" ");
+         for (char i = 'а'; i <= 'я'; ++i){
+             boolean isWrite = false;
+             for (int j = 0; j < strs.length; ++j){
+                 String tmp = strs[j];
+                 tmp = tmp.trim();
+                 if (tmp.charAt(0)==i) {
+                     System.out.print(" " + tmp);
+                     isWrite = true;
+                 }
+             }
+             if (isWrite)
+             System.out.println("");
+         }
+    }
 }
--- src/lab7/C.java
+++ src/lab7/C.java
@@ -1,10 +1,30 @@
 package lab7;
 
+import java.util.Scanner;
+
 /**
  * Created by apletea on 6.4.17.
  */
 public class C {
     public static void main(String [] args){
-
+        Scanner sc = new Scanner(System.in);
+        String fileName = sc.nextLine();
+        String mask =sc.nextLine();
+        for (int i = 0, j = 0 ; i < mask.length() && j <fileName.length() ;++i, ++j){
+            char sym = mask.charAt(i);
+            if (sym == '?')
+                continue;
+            if (sym == '*'){
+                while (j < fileName.length() &&  i+1 < mask.length() && fileName.charAt(j)!= mask.charAt(i+1) ){
+                    j++;
+                }
+                continue;
+            }
+            if (sym != fileName.charAt(j)){
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
     }
 }
