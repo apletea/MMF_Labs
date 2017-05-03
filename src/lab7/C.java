f22e23ad0f798d5df6a4ee67a8c1b0f4c12e965b
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-05-03 18:03:37 +0300

    push

--- src/lab7/C.java
+++ src/lab7/C.java
@@ -6,10 +6,8 @@ import java.util.Scanner;
  * Created by apletea on 6.4.17.
  */
 public class C {
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        String fileName = sc.nextLine();
-        String mask =sc.nextLine();
+
+    public static boolean isSatisfiedMask(String fileName, String mask){
         for (int i = 0, j = 0 ; i < mask.length() && j <fileName.length() ;++i, ++j){
             char sym = mask.charAt(i);
             if (sym == '?')
@@ -21,9 +19,18 @@ public class C {
                 continue;
             }
             if (sym != fileName.charAt(j)){
-                System.out.println("No");
-                return;
+                return false;
             }
+        }
+        return true;
+    }
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String fileName = sc.nextLine();
+        String mask =sc.nextLine();
+        for (int i = 0, j = 0 ; i < mask.length() && j <fileName.length() ;++i, ++j){
+          
         }
         System.out.println("Yes");
     }
