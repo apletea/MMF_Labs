aa1d7ef66374606cdc21dec24f83f648f892de6e
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-09-08 10:53:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,45 @@
 #include <vector>
 #include <stdlib.h>
 #include <ctime>
+#include <cstring>
 
 
 
-void merge_sort(std::vector<int> & arr ,int left, int right)
+void merge(std::vector<int> & arr, int l, int m, int r)
 {
+    int i, j, k;
+    int n1 = m - l +1;
+    int n2  = r - m;
     
+    int L[n1], R[n2];
+    
+    std::memcpy(L,arr.);
+    std::memcpy();
+    
+    i = 0;
+    j = 0;
+    k = l;
+    
+}
+
+
+void merge_sort(std::vector<int> & arr ,int l, int r)
+{
+    if (l < r)
+    {
+
+        int m = l+(r-l)/2;
+ 
+        merge_sort(arr, l, m);
+        merge_sort(arr, m+1, r);
+ 
+        merge(arr, l, m, r);
+    }
 }
 
 void quick_sort(std::vector<int>& arr,int left, int right,int N = 20, int depth = 0)
 {
-    
+
     if (depth > N)
         merge_sort(arr,left,right);
     int i = left, j = right;
@@ -46,26 +74,27 @@ int main()
     {
         arr[i] = std::rand()%(length*150)-50;
     }
+    int ans = 0;
+    int best_time = INT32_MAX;
+    quick_sort(arr,0,length-1,10000);
+//    while ( N < length)
+//    {
+//        std::clock_t start = std::clock();
+//        quick_sort(arr,0,length-1,N);
+//        double time = (double)(std::clock()-start)/CLOCKS_PER_SEC;
+
+//        if (best_time > time)
+//        {
+//            ans = start;
+//            best_time  = start;
+//        }
+//        std::cout << time << std::endl;
+//    }
+//    std::cout << "Best depth is" << ans << "  for array length of " << length <<   std::endl;
     for (auto num : arr)
     {
         std::cout << num << std::endl;
     }
-    int ans = 0;
-    int best_time = INT32_MAX;
-    while ( N < length)
-    {
-        std::clock_t start = std::clock();
-        quick_sort(arr,0,length-1,N);
-        double time = (double)(std::clock()-start)/CLOCKS_PER_SEC;
-        
-        if (best_time > time)
-        {
-            ans = start;
-            best_time  = start;
-        }
-        std::cout << time << std::endl;
-    }
-    std::cout << "Best depth is" << ans << "  for array length of " << length <<   std::endl;
     return 0;
 }
 
