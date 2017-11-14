9d94d4059092318139b1dda964a4c111eef3a5b4
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-11-14 16:09:35 +0300

    Update graphs.cpp

--- Algorithms/graphs.cpp
+++ Algorithms/graphs.cpp
@@ -1,28 +1,28 @@
-// ███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█ 
-// ████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█ 
-// ███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
-// ████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
-// ███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█ 
-// ████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█ 
-// ███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
-// ████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
-// ███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
-// █████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█ 
-// █████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ 
-// █████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ 
-// ████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██ 
-// ████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██ 
-// █████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██ 
-// █████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███ 
-// ██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███ 
-// ███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████ 
-// ███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████ 
-// ████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████ 
-// █████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████ 
-// ██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████ 
-// ███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████ 
-// ██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████ 
-// ███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
+//███████▓█████▓▓╬╬╬╬╬╬╬╬▓███▓╬╬╬╬╬╬╬▓╬╬▓█
+//████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+//███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+//████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+//███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+//████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+//███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+//████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+//███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+//█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+//█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+//█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+//████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+//████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+//█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+//█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+//██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+//███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+//███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+//████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+//█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+//██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+//███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+//██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+//███████████████▓▓▓██▓▓╬╬╬╬╬╬▓███████████
 #include <bits/stdc++.h>
 
 
@@ -31,25 +31,92 @@ namespace gf_graph
 class graph
 {
 public:
-    
+
     std::list<graph*> neignbohrs;
     int val;
-    
-    static graph* create_undirected()
+
+    graph(int val)
     {
-        return nullptr;
+        this->val = val;
     }
-    
-    static std::vector<std::vector> create_directed(int n)
+
+    static graph* create_for_euler()
     {
-        return nullptr;
+        std::map<int,graph*> mp;
+        for (int i = 1; i <=  6; ++i)
+        {
+            graph* tmp = new graph(i);
+            mp[i] = tmp;
+        }
+
+        for (int i = 1; i <= 6; ++i)
+        {
+            std::list<graph*> neighbors;
+            neighbors.push_back(mp[(i%6)+1]);
+            mp[i]->neignbohrs = neighbors;
+        }
+
+        for (int i = 1; i <= 6; ++i)
+        {
+            mp[i]->neignbohrs.push_back(mp[((i+4)%6)+1]);
+        }
+
+        return mp[1];
+
     }
-    
+
+    static graph* create_for_not_euler()
+    {
+        std::map<int,graph*> mp;
+        for (int i = 1; i <=  6; ++i)
+        {
+            graph* tmp = new graph(i);
+            mp[i] = tmp;
+        }
+
+        mp[1]->neignbohrs.push_back(mp[2]);
+        mp[2]->neignbohrs.push_back(mp[1]);
+        mp[2]->neignbohrs.push_back(mp[3]);
+        mp[3]->neignbohrs.push_back(mp[2]);
+        mp[4]->neignbohrs.push_back(mp[3]);
+        mp[3]->neignbohrs.push_back(mp[4]);
+        mp[4]->neignbohrs.push_back(mp[5]);
+        mp[5]->neignbohrs.push_back(mp[4]);
+        mp[5]->neignbohrs.push_back(mp[6]);
+        mp[6]->neignbohrs.push_back(mp[5]);
+        mp[6]->neignbohrs.push_back(mp[1]);
+        mp[1]->neignbohrs.push_back(mp[6]);
+
+        mp[1]->neignbohrs.push_back(mp[3]);
+        mp[3]->neignbohrs.push_back(mp[1]);
+        mp[3]->neignbohrs.push_back(mp[5]);
+        mp[5]->neignbohrs.push_back(mp[3]);
+        mp[5]->neignbohrs.push_back(mp[1]);
+        mp[1]->neignbohrs.push_back(mp[5]);
+        return mp[1];
+
+    }
+
+    static graph* create_bound(int n)
+    {
+        return {};
+    }
+
+    static graph* create_unbound(int n)
+    {
+        return {};
+    }
+
+    static graph* create_for_dijctra(std::map<std::pair<graph*,graph*>,int> & weights)
+    {
+
+    }
+
     static std::map<std::pair<int,int>,short int> get_maze()
     {
-        
+
     }
-    
+
     std::vector<graph*> find_euler_path()
     {
         std::set<graph*> is_visited;
@@ -58,23 +125,30 @@ public:
         std::stack<graph*> st;
         std::vector<graph*> ans;
         st.push(this);
-        while(!st.empty())
-        {
-            
-            graph* v = st.top();
-            for (auto neighbor : v->neignbohrs)
-            {
-                neighbor->neignbohrs.remove(v);
-                v->neignbohrs.remove(neighbor);
-                st.push(neighbor);
-            }
-            ans.push_back(v);
-            st.pop();
-        }
+        std::set<std::pair<graph*,graph*>> set;
+//        while(!st.empty())
+//        {
+
+//            graph* v = st.top();
+//            st.pop();
+//            for (auto neighbor: v->neignbohrs)
+//            {
+//                if (set.find(std::pair<graph*,graph*>{neighbor,v})!=set.end())
+//                    continue;
+//                set.insert(std::pair<graph*,graph*>{neighbor,v});
+//                set.insert(std::pair<graph*,graph*>{v,neighbor});
+//                st.push(neighbor);
+//            }
+//            if (st.size())
+//                ans.push_back(st.top());
+//            //            ans.push_back(v);
+//        }
+//        ans[ans.size()-1] = this;
+        find_euler(this,set,ans);
         return ans;
-        
+
     }
-    
+
     static bool is_bound_graph(int graph_size,graph* root)
     {
         std::stack<graph*> st;
@@ -91,12 +165,12 @@ public:
             {
                 st.push(neighbor);
             }
-            
+
         }
         return edges.size() == graph_size;
-    } 
-    
-    
+    }
+
+
     std::map<graph*, int> dejkstra(std::map<std::pair<graph*,graph*>,int> weights)
     {
         std::set<graph*> is_visited;
@@ -109,29 +183,31 @@ public:
         {
             v = st.top();
             st.pop();
+            if (is_visited.find(v)!=is_visited.end())
+                continue;
             for (auto neighbor : v->neignbohrs)
             {
                 if (res.find(neighbor)==res.end())
-                    res[neighbor] = weights[std::pair{v,neighbor}];
+                    res[neighbor] = weights[std::pair<graph*,graph*>{v,neighbor}];
                 else
-                    res[neighbor] = std::min(res[neighbor],weights[std::pair{v,neighbor}]);
+                    res[neighbor] = std::min(res[neighbor],weights[std::pair<graph*,graph*>{v,neighbor}]);
                 st.push(neighbor);
             }
             is_visited.insert(v);
-            
+
         }
         return res;
     }
-    
+
     static std::vector<int> find_path()
     {
-        
+
     }
 private:
     static int is_valid_euler(graph* edge ,std::set<graph*> & is_visitetd)
     {
         int ans = 0;
-        if (is_visitetd.find(edge)==is_visitetd.end())
+        if (is_visitetd.find(edge)!=is_visitetd.end())
             return 0;
         for(auto  neignhbor : edge->neignbohrs)
         {
@@ -140,8 +216,21 @@ private:
         }
         return ans+edge->neignbohrs.size()%2;
     }
-    
-    
+
+    void find_euler(graph* node,std::set<std::pair<graph*,graph*>> & set, std::vector<graph*> & res)
+    {
+        for (auto vertex : node->neignbohrs)
+        {
+            if (set.find(std::pair<graph*,graph*>{vertex,node})!=set.end())
+                continue;
+            set.insert(std::pair<graph*,graph*>{vertex,node});
+            set.insert(std::pair<graph*,graph*>{node,vertex});
+            find_euler(vertex,set,res);
+        }
+        res.push_back(node);
+    }
+
+
 };
 
 
@@ -150,5 +239,26 @@ private:
 
 int main(int argc, char ** argv)
 {
-    
+    //euler
+    //    gf_graph::graph* root=gf_graph::graph::create_for_euler();
+    //    std::vector<gf_graph::graph*> res =root->find_euler_path();
+    //    if (!res.size())
+    //        std::cout << "There are not ueler path" << std::endl;
+    //    for (auto v : res)
+    //    {
+    //        std::cout << v->val << " ";
+    //    }
+    //    std::cout << std::endl;
+    //not euler
+    gf_graph::graph* roots=gf_graph::graph::create_for_not_euler();
+    std::vector<gf_graph::graph*> ress =roots->find_euler_path();
+    if (!ress.size())
+        std::cout << "There are not ueler path" << std::endl;
+//    std::cout << 1 << " ";
+    for (auto v : ress)
+    {
+
+        std::cout << v->val << " ";
+    }
+
 }
