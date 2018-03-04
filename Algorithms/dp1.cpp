47af007700f605a563bd59814871dbee2d54558d
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-03-04 19:55:56 +0300

    Update dp1.cpp

--- Algorithms/dp1.cpp
+++ Algorithms/dp1.cpp
@@ -11,7 +11,6 @@ void convert_to_vec(const std::string & str, std::vector<int> & vec)
 }
 
 
-//dp[i] = max(dp[i-1],dp[i-5]+cost[i])
 int main(int argc, char ** argv)
 {
     if (argc < 3)
@@ -19,56 +18,56 @@ int main(int argc, char ** argv)
         std::cout << "./app input.txt output.txt" << std::endl;
         return 0;
     }
-
+    
     std::ifstream  in(argv[1]);
     std::ofstream out(argv[2]);
-
+    
     std::string xstring;
     std::string coststring;
-
+    
     std::vector<int> x;
     std::vector<int> cost;
-
+    
     int len = 0;
     int roadlen = 0;
-
+    
     std::map<int,int> mp;
-
+    std::map<int,std::vector<int>> set;
+    
     std::getline(in,xstring);
     std::getline(in,coststring);
-
+    
     convert_to_vec(xstring,x);
     convert_to_vec(coststring,cost);
-
+    
     len = x.size();
     for (int i = 0; i < len; ++i)
     {
         mp[x[i]] = cost[i];
-//        mp[x[i+1]] = cost[i+1];
-//        mp[x[i+2]] = cost[i+2];
+        //        mp[x[i+1]] = cost[i+1];
+        //        mp[x[i+2]] = cost[i+2];
     }
-
+    
     std::sort(x.begin(),x.end());
-    roadlen = *std::max_element(x.begin(),x.end());
-    std::vector<int> dp(roadlen+1,0);
-
-    for (int i = 1,j = 0,next = x[0]; i < roadlen+1; ++i)
+    std::vector<int> dp(x.size()+1,0);
+    
+    for (int i = 0; i < x.size();++i)
     {
-        dp[i] = dp[i-1];
-        if (i == next)
+        int k = i;
+        while (k--!=0 || x[i]-x[k]<5);
+        if (i > 0)
         {
-            if (i >= 5)
-                dp[i] = std::max(dp[i-1],dp[i-5]+mp[x[j]]);
-            else
-                dp[i] = std::max(dp[i],mp[x[j]]);
-            j++;
-            next = x[j];
+            dp[i] = std::max(dp[i-1],dp[k]+mp[x[i]]);
+        }
+        else
+        {
+            dp[i] = mp[x[i]];
         }
     }
-
-    out << dp[roadlen];
-
-
+    
+    out << dp[x.size] << std::endl;
+    
+    
     in.close();
     out.close();
 }
