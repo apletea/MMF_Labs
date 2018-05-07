f2ee900c772a7c57e3d0f7e230084943381a4d25
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-05-07 22:59:26 +0300

    Update slam.cpp

--- Course Projects/slam.cpp
+++ Course Projects/slam.cpp
@@ -17,10 +17,9 @@ int main(int argc, char ** argv)
 	
     vector<string> vstrImageFilenames;
     cv::VideoCapture vc(argv[1]);
-    std::ifstream in_data(argv[2]);
+    std::ifstream in_data(argv[3]);
     std::string str;
-    LoadImages(strFile, vstrImageFilenames, vTimestamps);
-
+    cv::Mat im;
 
 
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
@@ -34,51 +33,19 @@ int main(int argc, char ** argv)
     {
         vc >> im;
         in_data >> str;
-	double tframe;
 	
 
-#ifdef COMPILEDWITHC11
-        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-#else
-        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-#endif
 
         // Pass the image to the SLAM system
         SLAM.TrackMonocular(im,tframe);
 
-#ifdef COMPILEDWITHC11
-        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-#else
-        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-#endif
 
-        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-        vTimesTrack[ni]=ttrack;
-
-        // Wait to load the next frame
-      //  double T=0;
-      //  if(ni<nImages-1)
-      //      T = vTimestamps[ni+1]-tframe;
-      //  else if(ni>0)
-      //      T = tframe-vTimestamps[ni-1];
-
-      // if(ttrack<T)
-      //      usleep((T-ttrack)*1e6);
     }
 
     SLAM.Shutdown();
 
     // Tracking time statistics
     sort(vTimesTrack.begin(),vTimesTrack.end());
-    float totaltime = 0;
-    for(int ni=0; ni<nImages; ni++)
-    {
-        totaltime+=vTimesTrack[ni];
-    }
-    cout << "-------" << endl << endl;
-    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-    cout << "mean tracking time: " << totaltime/nImages << endl;
 
     // Save camera trajectory
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
