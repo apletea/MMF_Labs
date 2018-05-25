b07d9cda2a2fd188ef91ed780f5ac01906516c2d
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-05-25 19:44:43 +0300

    Update my_slam.cc

--- Course Project/slam_source/Examples/Monocular/my_slam.cc
+++ Course Project/slam_source/Examples/Monocular/my_slam.cc
@@ -7,7 +7,6 @@
 
 #include<System.h>
 #include<vector>
-using namespace std;
 
 void LoadImages(const string &strFile, vector<string> &vstrImageFilenames,
                 vector<double> &vTimestamps);
@@ -19,17 +18,16 @@ int main(int argc, char ** argv)
     cv::VideoCapture vc(argv[1]);
     std::ifstream in_data(argv[4]);
     std::string str;
-//    LoadImages(strFile, vstrImageFilenames, vTimestamps);
     int nImages = 1;
     cv::Mat im;
     std::vector<int> sensors;
 
     ORB_SLAM2::System SLAM(argv[3],argv[2],ORB_SLAM2::System::SENSORS,true);
     SLAM.mbActivateLocalizationMode = true;
-    vector<float> vTimesTrack;
+    std::vector<float> vTimesTrack;
 
-	cout << endl << "-------" << endl;
-    cout << "Start processing sequence ..." << endl;
+    std::cout << std::endl << "-------" << std::endl;
+    std::cout << "Start processing sequence ..." << std::endl;
 
     for(int ni=0; ni<1000000; ni++)
     {
@@ -39,43 +37,16 @@ int main(int argc, char ** argv)
 	    double tframe;
 	    sensors = convert2Vec(in_data);
 
-#ifdef COMPILEDWITHC11
-        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-#else
-        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-#endif
-
         // Pass the image to the SLAM system
         SLAM.TrackSensor(im,sensors,tframe);
         std::cout << "TrackMonocular" << std::endl;
 
-
-#ifdef COMPILEDWITHC11
-        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-#else
-        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-#endif
-
         double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-        // Wait to load the next frame
-      //  double T=0;
-      //  if(ni<nImages-1)
-      //      T = vTimestamps[ni+1]-tframe;
-      //  else if(ni>0)
-      //      T = tframe-vTimestamps[ni-1];
-
-      // if(ttrack<T)
-      //      usleep((T-ttrack)*1e6);
         nImages = ni;
     }
 
     SLAM.Shutdown();
 
-    // Tracking time statistics
-
-
-    // Save camera trajectory
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
 
     return 0;
